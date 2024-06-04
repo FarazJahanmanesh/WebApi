104c8a4ba4a5979260093d45912cb2fea4dc823a
faraz jahan manesh <farazebipax@gmail.com>
2024-06-04 09:55:27 +0330

    try to implement unit of work design pattern

---
+++ Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories;
+public class ProductRepository: IProductRepository
+{
+	private readonly DbContext _dbContext;
+	public ProductRepository(DbContext dbContext)
+	{
+		_dbContext = dbContext;
+	}
+}
--- Persistence/UnitOfWork.cs
+++ Persistence/UnitOfWork.cs
@@ -1,4 +1,15 @@
-﻿namespace Persistence;
+﻿using Domain.Interfaces.Repositories;
+using Persistence.Context;
+using Persistence.Repositories;
+
+namespace Persistence;
 public class UnitOfWork
 {
+    private ApplicationDbContext ApplicationDbContext { get; }
+    public UnitOfWork()
+	{
+
+	}
+    private IProductRepository _productRepository;
+    public IProductRepository ProductRepository => _productRepository ?? new ProductRepository(ApplicationDbContext);
 }
